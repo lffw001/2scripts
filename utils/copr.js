ded3f04015b5f854cd36fb336979d56fea29de62
sudojia <i@imzjw.cn>
2024-09-21 18:56:19 +0800

    refactor: Simplified output format

--- utils/copr.js
+++ utils/copr.js
@@ -3,4 +3,4 @@ function printLogo() {console.log(`                  _         _  _
  / __|| | | | / _\` | / _ \\ | || | / _\` |
  \\__ \\| |_| || (_| || (_) || || || (_| |
  |___/ \\__,_| \\__,_| \\___/_/ ||_| \\__,_|
-                         |__/           \n`);}function printScriptInfo(script, author, home, currentVersion, newVersion) {console.log(`【脚本作者】${author}`);console.log(`【脚本地址】${home}`);console.log(`【TG-频道】https://t.me/IiiiOOiiiiOO`);console.log(`【TG-群组】https://t.me/v_script`);console.log(`【当前版本】v${currentVersion}\n【最新版本】v${newVersion}`);console.log(`【最近更新】${script.changelog.date}`);console.log(`【更新内容】${script.changelog.changes}`);}module.exports = {printLogo, printScriptInfo};
+                         |__/           \n`);}function printScriptInfo(script, author, home, currentVersion, newVersion) {console.log(`【脚本作者】${author}\n【脚本地址】${home}\n【当前版本】v${currentVersion}\n【最新版本】v${newVersion}\n【最近更新】${script.changelog.date}\n【更新内容】${script.changelog.changes}`);}module.exports = {printLogo, printScriptInfo};
